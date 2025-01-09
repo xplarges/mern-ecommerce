3fe7e7afdd060a619288249a62277fc20b46d553
xplarges <72367904+xplarges@users.noreply.github.com>
2025-01-09 16:28:56 +0300

    Update Navbar

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,18 +1,22 @@
 import { Link, NavLink } from "react-router-dom";
+import React, { useState } from "react";
 
 import "./Navbar.css";
 
 export default function Navbar() {
   const siteName = "Site Name";
+  const [menuIsOpen, menuSetIsOpen] = useState(false);
   return (
     <nav>
-      <Link to="/" className="site-title">{siteName}</Link>
-      <div className="menu">
+      <Link to="/" className="site-title">
+        {siteName}
+      </Link>
+      <div className="menu" onClick={() => menuSetIsOpen(!menuIsOpen)}>
         <span></span>
         <span></span>
         <span></span>
       </div>
-      <ul>
+      <ul className={menuIsOpen ? "open" : ""}>
         <li>
           <NavLink to="/">Home</NavLink>
         </li>
