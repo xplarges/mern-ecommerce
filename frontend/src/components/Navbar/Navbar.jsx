35e42aa2bdad97720d38594bde9929d43126c36e
xplarges <72367904+xplarges@users.noreply.github.com>
2025-01-10 06:49:25 +0300

    Navbar updated

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -21,10 +21,7 @@ export default function Navbar() {
           <NavLink to="/">Home</NavLink>
         </li>
         <li>
-          <NavLink to="/login">Login</NavLink>
-        </li>
-        <li>
-          <NavLink to="/register">Register</NavLink>
+          <NavLink to="/login">Sign In</NavLink>
         </li>
       </ul>
     </nav>
